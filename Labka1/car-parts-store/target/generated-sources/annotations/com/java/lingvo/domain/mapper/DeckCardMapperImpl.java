a3a521c3dd69a7ecddb1faace51b5df352edea9f
Kawoonchik <sydor.anatolii@gmail.com>
2026-09-14 17:51:58 +0300

    docker didnt die

--- Labka1/car-parts-store/target/generated-sources/annotations/com/java/lingvo/domain/mapper/DeckCardMapperImpl.java
+++ Labka1/car-parts-store/target/generated-sources/annotations/com/java/lingvo/domain/mapper/DeckCardMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-09-14T16:48:05+0300",
+    date = "2026-09-14T17:23:00+0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 25.0.2 (Oracle Corporation)"
 )
 @Component
